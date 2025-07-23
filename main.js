2b168c825b27b626c8b4f05a887c47eaf5e72a07
Herson Cortes <36556595+Herson23@users.noreply.github.com>
2025-07-23 12:45:00 -0600

    added main js

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById('pato-block').style.backgroundColor = '#F8FF00';
+    },3000);
+    setTimeout(function() {
+        window.location.href = "./menu.html";
+    },2000);
+    
+});
